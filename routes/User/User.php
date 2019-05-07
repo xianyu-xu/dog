e9956521b2d9dc4327fb0af2dd870c85627c60e5
xu_jj <896636805@qq.com>
2019-05-07 21:48:21 +0800

    手机验证以及注册

--- routes/User/User.php
+++ routes/User/User.php
@@ -3,7 +3,7 @@
 
 Route::group(['prefix' => 'user', 'namespace' => 'User'], function () {
 
-    Route::post('phone','UserController@phone')->name('user.phone');
+    Route::get('phone','UserController@phone')->name('user.phone');
     Route::post('register','UserController@register')->name('user.register');
 
 });
