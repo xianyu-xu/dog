ca2e21a48633d14cd2de1c904f05b7347702e6c5
xu_jj <896636805@qq.com>
2019-05-13 18:44:28 +0800

    ''

--- routes/User/User.php
+++ routes/User/User.php
@@ -5,5 +5,5 @@ Route::group(['prefix' => 'user', 'namespace' => 'User'], function () {
 
     Route::get('phone','UserController@phone')->name('user.phone');
     Route::post('register','UserController@register')->name('user.register');
-    
+    Route::post('getinfo','UserController@getinfo')->name('user.getinfo');
 });
