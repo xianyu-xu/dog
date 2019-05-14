2d1d0ec52cbd336a79ffed172ceaab67a56e134a
xu_jj <896636805@qq.com>
2019-05-14 18:44:09 +0800

    宠物添加

--- app/Http/Controllers/Animal/AnimalController.php
+++ app/Http/Controllers/Animal/AnimalController.php
@@ -12,7 +12,6 @@ class AnimalController extends Controller
     public function animal_add(Request $request)
     {
         $data = $request->post('animal_data');
-        var_dump($data);die;
         $res = Animals::add($data);
 
         return json_encode($res);
