51ef7872283154a1643d2341474f62523709519b
xu_jj <896636805@qq.com>
2019-05-27 17:02:25 +0800

    0

--- app/Models/timeThings.php
+++ app/Models/timeThings.php
@@ -9,17 +9,23 @@ class timeThings extends Model
 {
     //
     protected $table='time_things';
-    public $timestamps = true;
+
     protected $guarded = []; //黑名单
+    
+    public $timestamps = true;
+    protected $fillable=[];//白名单
 
 
 
     public static function add($data)
     {
+        $arr = array();
+        
         if(empty($data)){
             $res =  ['stat'=>801,'message'=>'事件信息错误'.$data];
         }else{
-            $bool = DB::table('time_things')->insert($data);
+            $arr = json_decode($data);
+            $bool = DB::table('time_things')->insert($arr);
             if($bool)
             {
                 $res =  ['stat'=>800,'message'=>'时间添加成功'];
