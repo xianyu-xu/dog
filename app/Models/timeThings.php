dc9e40c1c518c45b3d9b2f5bf3d5faf442e4025b
xu_jj <896636805@qq.com>
2019-05-27 21:06:37 +0800

    0

--- app/Models/timeThings.php
+++ app/Models/timeThings.php
@@ -25,6 +25,7 @@ class timeThings extends Model
             $res =  ['stat'=>801,'message'=>'事件信息错误'.$data];
         }else{
             $arr = json_decode($data);
+            var_dump($arr);die;
             $bool = DB::table('time_things')->insert($arr);
             if($bool)
             {
